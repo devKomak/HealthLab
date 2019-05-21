3441d0acf89c6cca41c57f38832628ac7a116c97
devKomak <komcio96@gmail.com>
2019-05-21 21:03:02 +0200

    firebase

--- HealthLabProject/src/app/app.module.ts
+++ HealthLabProject/src/app/app.module.ts
@@ -9,15 +9,48 @@ import { MedicinesPage } from '../pages/medicines/medicines';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule }  from '@angular/router';
+import { LoginComponent } from './login.component';
+import { DashboardComponent } from './dashboard.component';
+import { AuthGuard } from './auth.guard';
+
+const ROUTES = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+];
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBbdfg98Zj4vLXCqm4p3H3vgpv_108vW18",
+    authDomain: "healthlab-100.firebaseapp.com",
+    databaseURL: "https://healthlab-100.firebaseio.com",
+    projectId: "healthlab-100",
+    storageBucket: "healthlab-100.appspot.com",
+    messagingSenderId: "141315663759",
+    appId: "1:141315663759:web:ebb47635f37d8abd"
+  };
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
-    MedicinesPage
+    MedicinesPage,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
+    AngularFireAuthModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -31,6 +64,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  ],
 })
 export class AppModule {}
