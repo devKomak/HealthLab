6adafc9a20ee9debfd1a347872ac670cf465d17e
s194157 <bartekodrobina@gmail.com>
2019-05-23 23:45:25 +0200

    Poprawa bledow w app.module.ts

--- HealthLabProject/src/app/app.module.ts
+++ HealthLabProject/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { Youtube } from '../pipes/youtube/youtube';
+import { YoutubePipe } from '../pipes/youtube/youtube';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -14,7 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     ListPage,
-    Youtube
+    YoutubePipe
   ],
   imports: [
     BrowserModule,
