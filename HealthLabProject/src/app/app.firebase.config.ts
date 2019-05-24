a4b5b1e9997e73f0f848c369e085b89622a1c078
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-24 14:53:47 +0200

    pierwszy etap prac nad firebase

---
+++ HealthLabProject/src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = var config {
+    apiKey: "AIzaSyCY38kKT31W-0fuuMUsxaODMUsy9p6mQ5s",
+    authDomain: "healthlab-35722.firebaseapp.com",
+    databaseURL: "https://healthlab-35722.firebaseio.com",
+    projectId: "healthlab-35722",
+    storageBucket: "healthlab-35722.appspot.com",
+    messagingSenderId: "1042642228031",
+    appId: "1:1042642228031:web:682ab677e810cd89"
+  };
--- HealthLabProject/src/app/app.module.ts
+++ HealthLabProject/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { FIREBASE_CONFIG } from "./app.firebase.config";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from "angularfire2";
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
