ce4cf9e32b1ed2c2138290cba73250f2a804f3b6
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-24 15:31:34 +0200

    poprawione bledy

--- HealthLabProject/src/app/app.module.ts
+++ HealthLabProject/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { HomePage } from '../pages/home/home';
 import { MyApp } from './app.component';
 import { ListPage } from '../pages/list/list';
 import { FIREBASE_CONFIG } from "./app.firebase.config";
@@ -10,10 +11,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireModule } from "angularfire2";
 
+
 @NgModule({
   declarations: [
     MyApp,
-    ListPage
+    ListPage,
+    HomePage,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,7 @@ import { AngularFireModule } from "angularfire2";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    //HomePage,
+    HomePage,
     ListPage
   ],
   providers: [
