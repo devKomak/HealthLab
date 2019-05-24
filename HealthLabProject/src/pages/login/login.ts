adbe0f425d278e65521d6cc7de8cc3d5e3a66f15
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-24 23:19:38 +0200

    domyslna strona domowa dodana po zalogowaniu

--- HealthLabProject/src/pages/login/login.ts
+++ HealthLabProject/src/pages/login/login.ts
@@ -25,7 +25,9 @@ export class LoginPage {
   async login (user: User) {
     try {
       const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
-      console.log(result);
+      if (result) {
+        this.navCtrl.push('HomePage')
+      }
     }
     catch(e) {
       console.error(e);
