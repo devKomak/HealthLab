1146c0c0eb219419dcd3dc4d0baa8f228437d403
1bendzi <32649585+1bendzi@users.noreply.github.com>
2019-05-25 13:54:43 +0200

    porawienie bledu

--- HealthLabProject/src/app/app.module.ts
+++ HealthLabProject/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { HomePage } from '../pages/home/home';
+//import { HomePage } from '../pages/home/home';
 import { MyApp } from './app.component';
 import { ListPage } from '../pages/list/list';
 import { FIREBASE_CONFIG } from "./app.firebase.config";
